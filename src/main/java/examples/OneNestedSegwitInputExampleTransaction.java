e92d7ffcde929d84e86fefa209048c34de5421d3
Humberto Cezar <skateforevis@hotmail.com>
2021-06-22 01:08:31 -0300

    add: nested segwit example

---
+++ src/main/java/examples/OneNestedSegwitInputExampleTransaction.java
@@ -0,0 +1,51 @@
+package examples;
+
+import bitcoinjava.*;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.util.ArrayList;
+
+public class OneNestedSegwitInputExampleTransaction {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        Security.addProvider(new BouncyCastleProvider());
+
+        String secret = "dee42df9945848a98209557e7222d018";
+        System.out.println("private key for p2sh-p2wpkh input: " + secret);
+        PrivateKey privateKey = new PrivateKey(new BigInteger(1, Hex.decode(secret)));
+        String address = Script.p2wpkhScript(Hash160.hashToHex(privateKey.getPublicKey().getCompressedPublicKey())).p2shAddress(AddressConstants.TESTNET_P2SH_ADDRESS_PREFIX);
+        System.out.println("address for p2sh-p2wpkh: " + address);
+
+        Script redeemScript = Script.p2wpkhScript(Hash160.hashToHex(privateKey.getPublicKey().getCompressedPublicKey()));
+
+        String txid = "909eb6e2c3b468bf5bdbd68a0bec4cf5e1c3a814f41467607fc49c279b71dba4";
+        TransactionInput transactionInput0 = new TransactionInput(
+            txid,
+            BigInteger.ZERO,
+            new Script(new ArrayList<>()),
+            new BigInteger(1, Hex.decode("ffffffff"))
+        );
+
+        ArrayList<TransactionInput> transactionInputArrayList = new ArrayList<>();
+        transactionInputArrayList.add(transactionInput0);
+
+        BigInteger amount = BigInteger.valueOf(98_000);
+        TransactionOutput transactionOutput = new TransactionOutput(amount, Script.p2shScript(Base58.decodeWithChecksumToHex(address)));
+        System.out.println("output 0 address: " + address);
+        System.out.println("output 0 amount: " + "98,000 satoshis");
+
+        ArrayList<TransactionOutput> transactionOutputArrayList = new ArrayList<>();
+        transactionOutputArrayList.add(transactionOutput);
+
+        Transaction transaction = new Transaction(BigInteger.ONE, transactionInputArrayList, transactionOutputArrayList, BigInteger.ZERO, true);
+        System.out.println("unsigned transaction: " + transaction.serialize());
+
+        P2SHTransactionECDSASigner.signNestedSegwit(transaction, privateKey, 0, redeemScript, BigInteger.valueOf(100_000));
+
+        System.out.println("signed transaction: " + transaction.serialize());
+    }
+}
--- src/main/java/examples/OneOfTwoMultisigP2SHInputExampleTransaction.java
+++ src/main/java/examples/OneOfTwoMultisigP2SHInputExampleTransaction.java
@@ -14,27 +14,22 @@ import java.util.List;
 import static bitcoinjava.OpCodes.*;
 import static java.math.BigInteger.valueOf;
 
-public class TwoOfTwoMultisigP2SHInputExample {
+public class OneOfTwoMultisigP2SHInputExampleTransaction {
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         Security.addProvider(new BouncyCastleProvider());
 
-        String secret = "f06812134dcf4ce5bb0dabd7718b1528";
+        String secret = "909f2d6fc6564407b73743b6871b70d9";
         System.out.println("private key 1 for p2sh input: " + secret);
         PrivateKey privateKey1 = new PrivateKey(new BigInteger(1, Hex.decode(secret)));
 
-        String secret2 = "ad7abc3d183d499392cc43a12561c924";
+        String secret2 = "3ab974f2e02e4275beb4e2440794d1ec";
         System.out.println("private key 2 for p2sh input: " + secret2);
         PrivateKey privateKey2 = new PrivateKey(new BigInteger(1, Hex.decode(secret2)));
 
-        Script redeemScript = new Script(List.of(
-            valueOf(OP_2),
-            privateKey1.getPublicKey().compressedPublicKeyHex(),
-            privateKey2.getPublicKey().compressedPublicKeyHex(),
-            valueOf(OP_2), valueOf(OP_CHECKMULTISIG)
-        ));
+        Script redeemScript = new Script(List.of(valueOf(OP_1), privateKey1.getPublicKey().compressedPublicKeyHex(), privateKey2.getPublicKey().compressedPublicKeyHex(), valueOf(OP_2), valueOf(OP_CHECKMULTISIG)));
         System.out.println("p2sh address: " + redeemScript.p2shAddress(AddressConstants.TESTNET_P2SH_ADDRESS_PREFIX));
 
-        String p2shInputTransactionId = "093b24dc0df9230a0e2a31471f2fafbb72943770bef62c09c5175e8b2396fae3";
+        String p2shInputTransactionId = "0f0e64bc96a42058e7fc0c172de37f12bdae1c276c16647a316adcf39248b850";
         TransactionInput transactionInput0 = new TransactionInput(
             p2shInputTransactionId,
             BigInteger.ZERO,
@@ -45,19 +40,17 @@ public class TwoOfTwoMultisigP2SHInputExample {
         ArrayList<TransactionInput> transactionInputArrayList = new ArrayList<>();
         transactionInputArrayList.add(transactionInput0);
 
-        BigInteger amount = BigInteger.valueOf(48_000);
+        BigInteger amount = BigInteger.valueOf(55_000);
         TransactionOutput transactionOutput = new TransactionOutput(amount, Script.p2wpkhScript(Bech32.decode("tb", "tb1q63rv8027mnhszkmf0f5qkxhk48r9tcyk0n6m8l")[1]));
         System.out.println("output 0 address: " + "tb1q63rv8027mnhszkmf0f5qkxhk48r9tcyk0n6m8l");
-        System.out.println("output 0 amount: " + "48,000 satoshis");
+        System.out.println("output 0 amount: " + "55,000 satoshis");
 
         ArrayList<TransactionOutput> transactionOutputArrayList = new ArrayList<>();
         transactionOutputArrayList.add(transactionOutput);
 
         Transaction transaction = new Transaction(BigInteger.ONE, transactionInputArrayList, transactionOutputArrayList, BigInteger.ZERO, false);
         System.out.println("unsigned transaction: " + transaction.serialize());
-
         P2SHTransactionECDSASigner.partialSign(transaction, privateKey1, 0, redeemScript);
-        P2SHTransactionECDSASigner.partialSign(transaction, privateKey2, 0, redeemScript);
         P2SHTransactionECDSASigner.appendRedeemScript(transaction,  0, redeemScript);
 
         System.out.println("signed transaction: " + transaction.serialize());
