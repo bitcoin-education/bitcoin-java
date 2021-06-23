186ac1bf0cac27849fc9cad0272bc61ed086f1c2
Humberto Cezar <skateforevis@hotmail.com>
2021-06-22 23:30:07 -0300

    add: mnemonic seed

--- src/main/java/bitcoinjava/Bech32.java
+++ src/main/java/bitcoinjava/Bech32.java
@@ -39,30 +39,7 @@ public class Bech32 {
     }
 
     public static List<Integer> convertBits(byte[] key, int fromBits, int toBits, boolean pad) {
-        BigInteger acc = ZERO;
-        BigInteger bits = ZERO;
-        List<Integer> result = new ArrayList<>();
-        BigInteger maxValue = ONE.shiftLeft(toBits).subtract(ONE);
-        BigInteger maxAcc = ONE.shiftLeft(fromBits + toBits - 1).subtract(ONE);
-        for (byte b : key) {
-            int bi = Byte.toUnsignedInt(b);
-            if (bi >> fromBits != 0) {
-                return null;
-            }
-            acc = acc.shiftLeft(fromBits).xor(valueOf(bi)).and(maxAcc);
-            bits = bits.add(valueOf(fromBits));
-            while (bits.compareTo(valueOf(toBits)) >= 0) {
-                bits = bits.subtract(valueOf(toBits));
-                result.add(acc.shiftRight(bits.intValueExact()).and(maxValue).intValueExact());
-            }
-        }
-        if (pad && !bits.equals(ZERO)) {
-            result.add(acc.shiftLeft(toBits - bits.intValueExact()).and(maxValue).intValueExact());
-        }
-        if (!pad && (bits.compareTo(valueOf(fromBits)) >= 0 || !acc.shiftLeft(valueOf(toBits).subtract(bits).intValueExact()).and(maxValue).equals(ZERO))) {
-            return null;
-        }
-        return result;
+        return BitsConverter.convertBits(key, fromBits, toBits, pad);
     }
 
     public static BigInteger polymod(List<Integer> values) {
